1266deeb0d6a9543400a939c841cf072086c3426
Korey M Stafford <kmstafford1@gmail.com>
2024-03-19 17:47:26 -0700

    initial commit

---
+++ dal.py
@@ -0,0 +1,516 @@
+import sqlite3
+from dataclasses import dataclass, asdict, fields, field
+import numpy as np
+import os
+from typing import Optional, Dict, Any, List
+
+DB_PATH = 'race.db'
+    
+class DCExtender:
+    
+    def get_fields(self):
+        return [field.name for field in fields(self)]
+    
+    @classmethod
+    def create_table_sql(self):
+        # TODO add doc string
+        # build create_table_sql
+        field_strings = []
+        for field in fields(self):
+            field_type = field.type
+            if field_type == int:
+                sql_type = 'INTEGER'
+            elif field_type == str:
+                sql_type = 'TEXT'
+            elif field_type == float:
+                sql_type = 'REAL'
+            elif field_type == bool:
+                sql_type = 'TEXT'
+            else:
+                raise ValueError(f"Unsupported data type: {field_type}")
+            field_strings.append(f"{field.name} {sql_type}")
+        fields_sql = ', '.join(field_strings)
+        sql = f"CREATE TABLE {self.__name__} ({fields_sql})"       
+        return sql 
+    
+    @classmethod
+    def create_table(self, connection):
+        # TODO add doc string
+        # get create table sql
+        sql = self.create_table_sql()
+        
+        # create table
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        connection.commit()
+        print('-'*5,f'CREATED TABLE {self.__name__}')
+        return None
+
+    @classmethod
+    def insert_statement(self): 
+        # TODO add doc string               
+        # get the list of fields
+        fields_list = self.get_fields(self)
+        fields_str = ", ".join(fields_list)
+        
+        # get the list of values
+        values = ["?" for f in fields_list]
+        values = ','.join(values)
+        
+        # create and return an insert statement with list of fields and values
+        insert_statement = f'INSERT INTO {self.__name__} ({fields_str}) VALUES ({values})'
+        return insert_statement      
+    
+    @classmethod
+    def get_session_data(self, sub_session_id):
+        # TODO add doc string
+        conn = sqlite3.connect(DB_PATH)
+        cursor = conn.cursor()
+        sql = f'SELECT * FROM {self.__name__} where sub_session_id = '+str(sub_session_id)
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        if len(rows) == 0:
+            return None
+        return rows
+    
+    @classmethod
+    def get_max_session_data(self,dict=True):
+        # TODO add doc string
+        conn = sqlite3.connect(DB_PATH)
+        cursor = conn.cursor()
+        sql = f'SELECT * FROM {self.__name__} where sub_session_id = (SELECT MAX(sub_session_id) from {self.__name__})'
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        return_data = []
+        if len(rows) == 0:
+            return None
+        for row in rows:
+            rd = self(*row)
+            if dict:
+                return_data.append(asdict(rd))
+            else:
+                return_data.append(rd)
+        return return_data
+    
+    @classmethod
+    def delete(self, init_datetime):
+        # TODO add doc string
+        conn = sqlite3.connect(DB_PATH)
+        cursor = conn.cursor()
+        sql = f'DELETE FROM {self.__name__} where InitDateTime = "{init_datetime}"'
+        cursor.execute(sql)
+        conn.commit()
+
+    def insert(self):
+        # TODO add doc string
+        conn = sqlite3.connect(DB_PATH)
+        cursor = conn.cursor()
+        insert_sql = self.insert_statement()
+        cursor.executemany(insert_sql,[self.unpack()])
+        conn.commit()
+          
+    def unpack(self):
+        # TODO add doc string
+        return tuple(getattr(self, field.name) for field in fields(self))
+    
+@dataclass
+class WeekendInfo(DCExtender):
+    InitDateTime: str
+    TrackName: str
+    TrackID: int
+    TrackLength: str
+    TrackLengthOfficial: str
+    TrackDisplayName: str
+    TrackDisplayShortName: str
+    TrackConfigName: str
+    TrackCity: str
+    TrackCountry: str
+    TrackAltitude: str
+    TrackLatitude: str
+    TrackLongitude: str
+    TrackNorthOffset: str
+    TrackNumTurns: int
+    TrackPitSpeedLimit: str
+    TrackType: str
+    TrackDirection: str
+    TrackWeatherType: str
+    TrackSkies: str
+    TrackSurfaceTemp: str
+    TrackAirTemp: str
+    TrackAirPressure: str
+    TrackWindVel: str
+    TrackWindDir: str
+    TrackRelativeHumidity: str
+    TrackFogLevel: str
+    TrackPrecipitation: str
+    TrackCleanup: int
+    TrackDynamicTrack: int
+    TrackVersion: str
+    SeriesID: int
+    SeasonID: int
+    SessionID: int
+    SubSessionID: int
+    LeagueID: int
+    Official: int
+    RaceWeek: int
+    EventType: str
+    Category: str
+    SimMode: str
+    TeamRacing: int
+    MinDrivers: int
+    MaxDrivers: int
+    DCRuleSet: str
+    QualifierMustStartRace: int
+    NumCarClasses: int
+    NumCarTypes: int
+    HeatRacing: int
+    BuildType: str
+    BuildTarget: str
+    BuildVersion: str
+
+@dataclass
+class ResultPosition(DCExtender):
+    InitDateTime: str
+    Position: int
+    ClassPosition: int
+    CarIdx: int
+    Lap: int
+    Time: float
+    FastestLap: int
+    FastestTime: float
+    LastTime: float
+    LapsLed: int
+    LapsComplete: int
+    JokerLapsComplete: int
+    LapsDriven: float
+    Incidents: int
+    ReasonOutId: int
+    ReasonOutStr: str
+
+@dataclass
+class Session(DCExtender):
+    InitDateTime: str
+    SessionNum: int
+    SessionLaps: str
+    SessionTime: str
+    SessionNumLapsToAvg: int
+    SessionType: str
+    SessionTrackRubberState: str
+    SessionName: str
+    SessionSubType: str
+    SessionSkipped: int
+    SessionRunGroupsUsed: int
+    SessionEnforceTireCompoundChange: int
+    #ResultsPositions: List[ResultPosition]
+
+@dataclass
+class RaceData(DCExtender):
+    Sessions: List[Session]
+
+@dataclass
+class Driver(DCExtender):
+    InitDateTime: str
+    CarIdx: int
+    UserName: str
+    AbbrevName: str
+    Initials: str
+    UserID: int
+    TeamID: int
+    TeamName: str
+    CarNumber: str
+    CarNumberRaw: int
+    CarPath: str
+    CarClassID: int
+    CarID: int
+    CarIsPaceCar: int
+    CarIsAI: int
+    CarIsElectric: int
+    CarScreenName: str
+    CarScreenNameShort: str
+    CarClassShortName: str
+    CarClassRelSpeed: int
+    CarClassLicenseLevel: int
+    CarClassMaxFuelPct: str
+    CarClassWeightPenalty: str
+    CarClassPowerAdjust: str
+    CarClassDryTireSetLimit: str
+    CarClassColor: int
+    CarClassEstLapTime: float
+    IRating: int
+    LicLevel: int
+    LicSubLevel: int
+    LicString: str
+    LicColor: int
+    IsSpectator: int
+    CarDesignStr: str
+    HelmetDesignStr: str
+    SuitDesignStr: str
+    BodyType: int
+    FaceType: int
+    HelmetType: int
+    CarNumberDesignStr: str
+    CarSponsor_1: int
+    CarSponsor_2: int
+    #ClubName: str
+    #ClubID: int
+    #DivisionName: str
+    #DivisionID: int
+    CurDriverIncidentCount: int
+    TeamIncidentCount: int
+
+@dataclass
+class SessionWeather(DCExtender):
+    AirDensity: float
+    AirPressure: float
+    AirTemp: float
+    FogLevel: float
+    Precipitation: float
+    RelativeHumidity: float
+    Skies: int
+    SolarAltitude: float
+    SolarAzimuth: float
+    WeatherType: str
+    WeatherVersion: str
+    WindDir: float
+    WindVel: float
+
+@dataclass
+class LapTiming(DCExtender):
+    InitDateTime: str
+    Lap: int
+    LapBestLap: int
+    LapBestLapTime: float
+    LapBestNLapLap: int
+    LapBestNLapTime: float
+    LapCompleted: int
+    LapCurrentLapTime: float
+    LapDeltaToBestLap: float
+    LapDeltaToBestLap_DD: float
+    LapDeltaToBestLap_OK: bool
+    LapDeltaToOptimalLap: float
+    LapDeltaToOptimalLap_DD: float
+    LapDeltaToOptimalLap_OK: bool
+    LapDeltaToSessionBestLap: float
+    LapDeltaToSessionBestLap_DD: float
+    LapDeltaToSessionBestLap_OK: bool
+    LapDeltaToSessionLastlLap: float
+    LapDeltaToSessionLastlLap_DD: float
+    LapDeltaToSessionLastlLap_OK: bool
+    LapDeltaToSessionOptimalLap: float
+    LapDeltaToSessionOptimalLap_DD: float
+    LapDeltaToSessionOptimalLap_OK: bool
+    LapDist: float
+    LapDistPct: float
+    LapLasNLapSeq: int
+    LapLastLapTime: float
+    LapLastNLapTime: float
+
+@dataclass
+class Tires(DCExtender):
+    InitDateTime: str
+    # Temperature Data
+    LFtempCL: float
+    LFtempCM: float
+    LFtempCR: float
+    LRtempCL: float
+    LRtempCM: float
+    LRtempCR: float
+    RFtempCL: float
+    RFtempCM: float
+    RFtempCR: float
+    RRtempCL: float
+    RRtempCM: float
+    RRtempCR: float
+
+    # Wear Data
+    LFwearL: float
+    LFwearM: float
+    LFwearR: float
+    LRwearL: float
+    LRwearM: float
+    LRwearR: float
+    RFwearL: float
+    RFwearM: float
+    RFwearR: float
+    RRwearL: float
+    RRwearM: float
+    RRwearR: float
+
+    #Other
+    TireLF_RumblePitch: float
+    TireLR_RumblePitch: float
+    TireRF_RumblePitch: float
+    TireRR_RumblePitch: float
+    TireSetsAvailable: int
+    TireSetsUsed: int
+    FrontTireSetsAvailable: int
+    FrontTireSetsUsed: int
+    LeftTireSetsAvailable: int
+    LeftTireSetsUsed: int
+    LFTiresAvailable: int
+    LFTiresUsed: int
+    RFTiresAvailable: int
+    RFTiresUsed: int
+    RRTiresAvailable: int
+    RRTiresUsed: int
+
+@dataclass    
+class RacingTelemetryData(DCExtender):
+    #lap
+    InitDateTime: str
+    SessionTime: float
+    Lap: int
+    LapDist: float
+    LapDistPct: float
+    
+    # Brake Data
+    Brake: float
+    BrakeABSactive: bool
+    BrakeRaw: float 
+
+    # Gear and Handbrake
+    Gear: int
+    HandbrakeRaw: float   
+
+    # Clutch Data
+    Clutch: float
+    ClutchRaw: float
+
+    # Steering and Throttle Data
+    SteeringWheelAngle: float
+    SteeringWheelAngleMax: float
+    SteeringWheelLimiter: float
+    SteeringWheelMaxForceNm: float
+    SteeringWheelPctDamper: float
+    SteeringWheelPctIntensity: float
+    SteeringWheelPctSmoothing: float
+    SteeringWheelPctTorque: float
+    SteeringWheelPctTorqueSign: float
+    SteeringWheelPctTorqueSignStops: float
+    SteeringWheelPeakForceNm: float
+    SteeringWheelTorque: float
+    #SteeringWheelTorque_ST: float
+    Throttle: float
+    ThrottleRaw: float
+
+    # Fuel Data
+    FuelLevel: float
+    FuelLevelPct: float
+    FuelPress: float
+    FuelUsePerHour: float    
+
+    #Track Temp
+    TrackTempCrew: float
+    '''
+    # Driver Control Data
+    DCDriversSoFar: int
+    DCLapStatus: str
+    dcPitSpeedLimiterToggle: str
+    dcStarter: str
+
+    # Display and Repair Options
+    DisplayUnits: int
+    dpFastRepair: str
+    dpFuelAddKg: float
+    dpFuelAutoFillActive: str
+    dpFuelAutoFillEnabled: str
+    dpFuelFill: str
+    dpLFTireChange: str
+    dpLFTireColdPress: float
+    dpLRTireChange: str
+    dpLRTireColdPress: float
+    dpRFTireChange: str
+    dpRFTireColdPress: float
+    dpRRTireChange: str
+    dpRRTireColdPress: float
+    dpWindshieldTearoff: str    
+
+    # Engine Data
+    Engine0_RPM: float
+    EngineWarnings: int
+    EnterExitReset: int
+    FastRepairAvailable: int
+    FastRepairUsed: int   
+
+    # Track and Garage Data
+    IsGarageVisible: str
+    IsInGarage: str
+    IsOnTrack: str
+    IsOnTrackCar: str            
+    OnPitRoad: str
+
+    # Acceleration Data
+    LatAccel: float
+    LatAccel_ST: float
+    LongAccel: float
+    LongAccel_ST: float
+
+    # Shock and Pressure Data
+    LFbrakeLinePress: float
+    LFcoldPressure: float
+    LFshockDefl: float
+    LFshockDefl_ST: float
+    LFshockVel: float
+    LFshockVel_ST: float
+    LRbrakeLinePress: float
+    LRcoldPressure: float
+    LRshockDefl: float
+    LRshockDefl_ST: float
+    LRshockVel: float
+    LRshockVel_ST: float
+    
+    # Pit and Repair Data
+    PitOptRepairLeft: float
+    PitRepairLeft: float
+    PitSvFlags: int
+    PitSvFuel: float
+    PitSvLFP: float
+    PitSvLRP: float
+    PitSvRFP: float
+    PitSvRRP: float
+    PitSvTireCompound: str
+
+    VelocityX: float
+    VelocityX_ST: float
+    VelocityY: float
+    VelocityY_ST: float
+    VelocityZ: float
+    VelocityZ_ST: float
+    VertAccel: float
+    VertAccel_ST: float
+    VidCapActive: str
+    VidCapEnabled: str
+    Voltage: float
+    WaterLevel: float
+    WaterTemp: float
+    WindDir: float
+    WindVel: float
+    Yaw: float
+    YawNorth: float
+    YawRate: float
+    YawRate_ST: float
+    '''
+    
+def setup_db(db_path=DB_PATH, new_db=True):
+    # TODO add docstring
+    if new_db:
+        #TODO add logging
+        print('---CREATING NEW TELEMETRY DATABASE---')
+        if os.path.exists(db_path):            
+            # remove db
+            os.remove(db_path)                        
+            #create new db and tables tables
+            conn = sqlite3.connect(DB_PATH)
+            Session.create_table(conn)
+            WeekendInfo.create_table(conn)
+            Driver.create_table(conn)
+            ResultPosition.create_table(conn)
+            RacingTelemetryData.create_table(conn)
+            LapTiming.create_table(conn)
+
+            
+    conn = sqlite3.connect(DB_PATH)
+    conn.close()
+
+if __name__ == '__main__':
+    print(DB_PATH)
---
+++ reader.py
@@ -0,0 +1,279 @@
+import irsdk
+import time
+from dataclasses import dataclass, field, asdict
+from datetime import datetime
+import time
+import logging
+import dal
+
+DB_RECREATE = True
+DEV_LOG = False
+
+if DEV_LOG:
+    # Configure logging to write to a file, include the time, and set level to DEBUG
+    logging.basicConfig(filename='reader.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def log(message):
+    '''Log message for debugging if DEV_LOG is true.'''
+    if DEV_LOG:
+        logging.debug(message)
+
+#this is our State class, with some helpful variables
+class State:
+    ir_connected = False
+    last_car_setup_tick = -1
+    init_time = None
+    weekend_info = False
+    init_datetime = None
+
+def check_iracing():
+    '''Checks for an active connection to iRacing. This should suppot multiple sessions being opened and closed.'''    
+    
+    if state.ir_connected and not (ir.is_initialized and ir.is_connected):
+        # don't forget to reset your State variables
+        state.ir_connected = False
+        state.last_car_setup_tick = -1
+        state.init_datetime = None
+        state.weekend_info = False
+        # we are shutting down ir library (clearing all internal variables)
+        ir.shutdown()
+        log('IRSDK Disconnected')
+    
+    elif not state.ir_connected and ir.startup() and ir.is_initialized and ir.is_connected:
+        state.ir_connected = True
+        # the init time is what we use to show that a new session has been initialized and we are using that time
+        # as a unique identifier to track different sessions, this is because practice sessions run locally set
+        # all the session id values to 0
+        
+        # Get the current UTC date and time
+        current_utc_datetime = datetime.utcnow()
+        
+        # Format the date and time as a string
+        formatted_utc_datetime = current_utc_datetime.strftime("%Y-%m-%d %H:%M:%S")
+        state.init_datetime = formatted_utc_datetime
+
+def log_telemetry(loop_counter):
+    # time each loop to make sure we can hit <1/60th a second
+    start_time = time.perf_counter()
+    
+    # you need this to freeze buffer for telemetry
+    ir.freeze_var_buffer_latest()
+    t = ir['SessionTime'] 
+    
+    # some data we only store every 10 loops, this keeps the program running quickly
+    if loop_counter == 0 or loop_counter % 10 == 0:
+        # what we can do here is check for a change in the weekend info, but just store it once
+        # we create a key that connects this telemetry this weekend info with any other info logged during the session 
+        weekend_info = ir['WeekendInfo']
+        log(weekend_info)
+        
+        # if we haven't logged the weekend info then log it, should only need to log it once
+        if not state.weekend_info:
+            weekend_info.pop('WeekendOptions')
+            weekend_info.pop('TelemetryOptions')
+            weekend = dal.WeekendInfo(state.init_datetime, **weekend_info)
+            log(weekend) 
+            weekend.insert()   
+            state.weekend_info = weekend_info
+        
+        # grab the session info, get new each loop.
+        session_info = ir['SessionInfo']
+        log(session_info)
+        
+        # per session get session object and results position
+        for s in session_info['Sessions']:
+            session = dal.Session(InitDateTime = state.init_datetime,
+                            SessionNum=s['SessionNum'],
+                            SessionLaps=s['SessionLaps'],
+                            SessionTime=s['SessionTime'],
+                            SessionNumLapsToAvg=s['SessionNumLapsToAvg'],
+                            SessionType=s['SessionType'],
+                            SessionTrackRubberState=s['SessionTrackRubberState'],
+                            SessionName=s['SessionName'],
+                            SessionSubType=s['SessionSubType'],
+                            SessionSkipped=s['SessionSkipped'],
+                            SessionRunGroupsUsed=s['SessionRunGroupsUsed'],
+                            SessionEnforceTireCompoundChange=s['SessionEnforceTireCompoundChange'])
+            log(session)
+            # session we just want to overwrite what we have
+            # we delete the current session for the current init_datetime
+            # then insert new
+            session.delete(state.init_datetime)
+            session.insert()
+            
+            dal.ResultPosition.delete(state.init_datetime)
+            
+            if 'ResultsPosition' in s:
+                for r in s['ResultsPositions']:
+                    result_position = dal.ResultPosition(state.init_datetime,**r)
+                    log(result_position)
+                    result_position.insert()
+
+        # get info on drivers
+        dal.Driver.delete(state.init_datetime)
+        for d in ir['DriverInfo']['Drivers']:
+            driver = dal.Driver(state.init_datetime,**d)
+            driver.insert()
+            log(driver)
+        
+        # get SessionWeather
+        # TODO add
+        
+        # get LapTiming
+        lap_timing = dal.LapTiming(
+            InitDateTime = state.init_datetime,
+            Lap=ir["Lap"],
+            LapBestLap=ir["LapBestLap"],
+            LapBestLapTime=ir["LapBestLapTime"],
+            LapBestNLapLap=ir["LapBestNLapLap"],
+            LapBestNLapTime=ir["LapBestNLapTime"],
+            LapCompleted=ir["LapCompleted"],
+            LapCurrentLapTime=ir["LapCurrentLapTime"],
+            LapDeltaToBestLap=ir["LapDeltaToBestLap"],
+            LapDeltaToBestLap_DD=ir["LapDeltaToBestLap_DD"],
+            LapDeltaToBestLap_OK=ir["LapDeltaToBestLap_OK"],
+            LapDeltaToOptimalLap=ir["LapDeltaToOptimalLap"],
+            LapDeltaToOptimalLap_DD=ir["LapDeltaToOptimalLap_DD"],
+            LapDeltaToOptimalLap_OK=ir["LapDeltaToOptimalLap_OK"],
+            LapDeltaToSessionBestLap=ir["LapDeltaToSessionBestLap"],
+            LapDeltaToSessionBestLap_DD=ir["LapDeltaToSessionBestLap_DD"],
+            LapDeltaToSessionBestLap_OK=ir["LapDeltaToSessionBestLap_OK"],
+            LapDeltaToSessionLastlLap=ir["LapDeltaToSessionLastlLap"],
+            LapDeltaToSessionLastlLap_DD=ir["LapDeltaToSessionLastlLap_DD"],
+            LapDeltaToSessionLastlLap_OK=ir["LapDeltaToSessionLastlLap_OK"],
+            LapDeltaToSessionOptimalLap=ir["LapDeltaToSessionOptimalLap"],
+            LapDeltaToSessionOptimalLap_DD=ir["LapDeltaToSessionOptimalLap_DD"],
+            LapDeltaToSessionOptimalLap_OK=ir["LapDeltaToSessionOptimalLap_OK"],
+            LapDist=ir["LapDist"],
+            LapDistPct=ir["LapDistPct"],
+            LapLasNLapSeq=ir["LapLasNLapSeq"],
+            LapLastLapTime=ir["LapLastLapTime"],
+            LapLastNLapTime=ir["LapLastNLapTime"]
+        )
+        lap_timing.insert()
+        
+        # get TireData
+        # Manually creating an instance of Tires
+        tires = dal.Tires(
+            InitDateTime = state.init_datetime,
+            LFtempCL=ir["LFtempCL"],
+            LFtempCM=ir["LFtempCM"],
+            LFtempCR=ir["LFtempCR"],
+            LRtempCL=ir["LRtempCL"],
+            LRtempCM=ir["LRtempCM"],
+            LRtempCR=ir["LRtempCR"],
+            RFtempCL=ir["RFtempCL"],
+            RFtempCM=ir["RFtempCM"],
+            RFtempCR=ir["RFtempCR"],
+            RRtempCL=ir["RRtempCL"],
+            RRtempCM=ir["RRtempCM"],
+            RRtempCR=ir["RRtempCR"],
+            LFwearL=ir["LFwearL"],
+            LFwearM=ir["LFwearM"],
+            LFwearR=ir["LFwearR"],
+            LRwearL=ir["LRwearL"],
+            LRwearM=ir["LRwearM"],
+            LRwearR=ir["LRwearR"],
+            RFwearL=ir["RFwearL"],
+            RFwearM=ir["RFwearM"],
+            RFwearR=ir["RFwearR"],
+            RRwearL=ir["RRwearL"],
+            RRwearM=ir["RRwearM"],
+            RRwearR=ir["RRwearR"],
+            TireLF_RumblePitch=ir["TireLF_RumblePitch"],
+            TireLR_RumblePitch=ir["TireLR_RumblePitch"],
+            TireRF_RumblePitch=ir["TireRF_RumblePitch"],
+            TireRR_RumblePitch=ir["TireRR_RumblePitch"],
+            TireSetsAvailable=ir["TireSetsAvailable"],
+            TireSetsUsed=ir["TireSetsUsed"],
+            FrontTireSetsAvailable=ir["FrontTireSetsAvailable"],
+            FrontTireSetsUsed=ir["FrontTireSetsUsed"],
+            LeftTireSetsAvailable=ir["LeftTireSetsAvailable"],
+            LeftTireSetsUsed=ir["LeftTireSetsUsed"],
+            LFTiresAvailable=ir["LFTiresAvailable"],
+            LFTiresUsed=ir["LFTiresUsed"],
+            RFTiresAvailable=ir["RFTiresAvailable"],
+            RFTiresUsed=ir["RFTiresUsed"],
+            RRTiresAvailable=ir["RRTiresAvailable"],
+            RRTiresUsed=ir["RRTiresUsed"]
+        )
+
+    # the main telem object for the player
+    racing_telemetry_data = dal.RacingTelemetryData(
+        InitDateTime=state.init_datetime,
+        SessionTime=t,
+        Lap=ir["Lap"],
+        LapDist=ir["LapDist"],
+        LapDistPct=ir["LapDistPct"],
+        Brake=ir["Brake"],
+        BrakeABSactive=ir["BrakeABSactive"],
+        BrakeRaw=ir["BrakeRaw"],
+        Clutch=ir["Clutch"],
+        ClutchRaw=ir["ClutchRaw"],
+        FuelLevel=ir["FuelLevel"],
+        FuelLevelPct=ir["FuelLevelPct"],
+        FuelPress=ir["FuelPress"],
+        FuelUsePerHour=ir["FuelUsePerHour"],
+        Gear=ir["Gear"],
+        HandbrakeRaw=ir["HandbrakeRaw"],
+        SteeringWheelAngle=ir["SteeringWheelAngle"],
+        SteeringWheelAngleMax=ir["SteeringWheelAngleMax"],
+        SteeringWheelLimiter=ir["SteeringWheelLimiter"],
+        SteeringWheelMaxForceNm=ir["SteeringWheelMaxForceNm"],
+        SteeringWheelPctDamper=ir["SteeringWheelPctDamper"],
+        SteeringWheelPctIntensity=ir["SteeringWheelPctIntensity"],
+        SteeringWheelPctSmoothing=ir["SteeringWheelPctSmoothing"],
+        SteeringWheelPctTorque=ir["SteeringWheelPctTorque"],
+        SteeringWheelPctTorqueSign=ir["SteeringWheelPctTorqueSign"],
+        SteeringWheelPctTorqueSignStops=ir["SteeringWheelPctTorqueSignStops"],
+        SteeringWheelPeakForceNm=ir["SteeringWheelPeakForceNm"],
+        SteeringWheelTorque=ir["SteeringWheelTorque"],
+        #SteeringWheelTorque_ST=ir["SteeringWheelTorque_ST"],
+        Throttle=ir["Throttle"],
+        ThrottleRaw=ir["ThrottleRaw"],
+        TrackTempCrew=ir["TrackTempCrew"],
+    )
+    
+    #for field_name, field_value in asdict(racing_telemetry_data).items():
+        #print(f"{field_name}: {field_value}")
+    log(racing_telemetry_data)
+    racing_telemetry_data.insert()
+    
+    # check loop performance
+    end_time = time.perf_counter()
+    time_taken = end_time - start_time
+    log(f'Main loop took: {time_taken}')
+
+if __name__ == '__main__':    
+    
+    # initializing ir and state
+    ir = irsdk.IRSDK()
+    state = State()
+    
+    # rebuild db if necessary
+    if DB_RECREATE:
+        dal.setup_db()
+    
+    try:
+        loop_counter = 0
+        # infinite loop
+        while True:
+            current_time= time.strftime("%H:%M:%S")
+
+            # check if we are connected to iracing
+            print(f"\rChecking for active iRacing session, last checked {current_time}", end="")
+            check_iracing()
+            
+            # if we are, then process data
+            if state.ir_connected:
+                log(f'iRacing session found, logging current session at {current_time}')
+                log_telemetry(loop_counter)
+            
+            # sleep for 1 second
+            # maximum you can use is 1/60
+            # cause iracing updates data with 60 fps
+            loop_counter += 1
+            time.sleep(.2)
+    
+    except KeyboardInterrupt:
+        pass
